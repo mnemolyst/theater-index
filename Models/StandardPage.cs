7230a4ce20e6ef85c7d7282ac4b256f769cc8abe
Joshua Salisbury <joshua.l.salisbury@gmail.com>
2025-06-21 15:42:20 -0500

    Initial commit

---
+++ Models/StandardPage.cs
@@ -0,0 +1,9 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace TheaterIndex.Models;
+
+[PageType(Title = "Standard page")]
+public class StandardPage  : Page<StandardPage>
+{
+}
